2f2a48f369206ff7af0e19136179d3e6c9163ddc
Nolan <112602295+NolanSmug@users.noreply.github.com>
2022-09-02 23:15:51 -0400

    Lab 1: initial setup

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Nolan's CS008 Website</title>
+        <meta name="author" content="Nolan Cyr">
+        <meta name="description" content="A site map to all of my Computer Science assignments for CS 008">
+        <style>
+            figure{
+                float: left; 
+                text-align: center; 
+                width: 20%;    
+            }
+            
+            img{
+                max-width: 100%;
+            }
+        </style>
+    </head>
+
+    <body>
+        <figure>
+            <img alt="Nolan Cyr circa 2022" src="image/nolan-cyr.png">
+            <figcaption>Walking around Roosevelt Island in NYC</figcaption>
+        </figure>
+
+        <h1>CS 008-<em>A</em> Fall 2022</h1>
+        <h2>Nolan Cyr - Site map</h2>
+        <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
+
+        <h2>Lab one - Setting up your sitemap.</h2>
+        <p><a href="test.php">Testing Page</a></p>
+    </body>
+</html>
