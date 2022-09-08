8b52cd8494086818b1f64a1ceaaf05d25d37fb6d
Nolan <112602295+NolanSmug@users.noreply.github.com>
2022-09-08 12:01:38 -0400

    Lab 2: updated site map

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             figure{
                 float: left; 
                 text-align: center; 
-                width: 20%;    
+                width: 20%;  
             }
             
             img{
@@ -26,9 +26,29 @@
 
         <h1>CS 008-<em>A</em> Fall 2022</h1>
         <h2>Nolan Cyr - Site map</h2>
+        <h3>Supporting Files</h3>
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
 
+
         <h2>Lab one - Setting up your sitemap</h2>
+        <h3>Public Files</h3>
         <p><a href="test.php">Testing Page</a></p>
+
+        <section class="grid-layout">
+            <h2 class="header">Lab Two - Choosing your topic.</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab2/index.php">index.php</a></p>      
+            </section>
+
+            <section class="supporting-files">
+                <h3>Supporting files</h3>
+            </section>
+
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+            </section>
+        </section>
+
     </body>
 </html>
