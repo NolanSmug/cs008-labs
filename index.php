e32c2ae3bb23c43b63807d54a2f8af6b8bb11bf8
Nolan <112602295+NolanSmug@users.noreply.github.com>
2022-09-15 18:20:17 -0400

    Lab 3: updated sitemap

--- index.php
+++ index.php
@@ -26,14 +26,32 @@
 
         <h1>CS 008-<em>A</em> Fall 2022</h1>
         <h2>Nolan Cyr - Site map</h2>
-        <h3>Supporting Files</h3>
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
 
+<!----------------------------Lab 3------------------------------------>
+        <section class="grid-layout">
+            <h2 class="header">Lab Three - Adding detail and a form.</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab3/detail.php">detail.php</a></p> 
+		        <p><a href="lab3/form.php">form.php</a></p> 
+		        <p><a href="lab3/index.php">index.php</a></p>
+            </section>
 
-        <h2>Lab one - Setting up your sitemap</h2>
-        <h3>Public Files</h3>
-        <p><a href="test.php">Testing Page</a></p>
+            <section class="supporting-files">
+                <h3>Supporting files</h3>
+                <p><a href="lab3/css/custom.css">custom.css</a> </p>
+                <p><a href="lab3/images/wireframe.png">wireframe.png</a></p>
+            </section>
 
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section>
+        </section>
+<!----------------------------Lab 3------------------------------------>
+
+<!----------------------------Lab 2------------------------------------>
         <section class="grid-layout">
             <h2 class="header">Lab Two - Choosing your topic.</h2>
             <section class="public-files">
@@ -49,6 +67,17 @@
                 <h3>Notes to grader</h3>
             </section>
         </section>
+<!----------------------------Lab 2------------------------------------>
+
+<!----------------------------Lab 1------------------------------------>
+        <section class="grid-layout">
+            <h2>Lab one - Setting up your sitemap</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="test.php">Testing Page</a></p>
+            </section>
+        </section>
+<!----------------------------Lab 1------------------------------------>
 
     </body>
 </html>
