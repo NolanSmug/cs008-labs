0cb33ed56f4ed191e2d8a9be34b3bd90ccd914ca
Nolan <112602295+NolanSmug@users.noreply.github.com>
2022-09-29 00:54:15 -0400

    Lab 4: updated site map

--- index.php
+++ index.php
@@ -6,20 +6,45 @@
         <meta name="author" content="Nolan Cyr">
         <meta name="description" content="A site map to all of my assignments for CS 008">
         <style>
-            figure{
-                float: left; 
-                text-align: center; 
-                width: 20%;  
-            }
             
-            img{
-                max-width: 100%;
+            body{
+                margin: auto;
+                padding: 3%;
+                width: 90%;
             }
+
+            figure {
+                border: thin solid darkslategray;
+                border-radius: 3%;
+                padding: 3%;
+                text-align: center;
+            }
+
+            figcaption {
+                color: #839e99;
+                font-style: italic;
+                text-align: center;
+            }
+
+            img{
+                border-radius: 3%;
+                max-width: 100%
+            }
+
+            .right{
+                float: right;
+                margin-left: 3%;
+            }
+
+            .small {
+                width: 20%;
+            }
+    
         </style>
     </head>
 
     <body>
-        <figure>
+        <figure class="right small">
             <img alt="Nolan Cyr circa 2022" src="image/nolan-cyr.png">
             <figcaption>Walking around Roosevelt Island in NYC</figcaption>
         </figure>
@@ -27,6 +52,29 @@
         <h1>CS 008-<em>A</em> Fall 2022</h1>
         <h2>Nolan Cyr - Site map</h2>
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
+        
+<!----------------------------Lab 4------------------------------------>
+        <section class="grid-layout">
+            <h2 class="header">Lab Four- Basic CSS</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab4/detail.php">detail.php</a></p> 
+		        <p><a href="lab4/form.php">form.php</a></p> 
+		        <p><a href="lab4/index.php">index.php</a></p>
+            </section>
+
+            <section class="supporting-files">
+                <h3>Supporting files</h3>
+                <p><a href="lab4/css/custom.css">custom.css</a> </p>
+                <p><a href="lab4/image/wireframe.png">wireframe.png</a></p>
+            </section>
+
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section>
+        </section>
+<!----------------------------Lab 4------------------------------------>
 
 <!----------------------------Lab 3------------------------------------>
         <section class="grid-layout">
@@ -41,7 +89,7 @@
             <section class="supporting-files">
                 <h3>Supporting files</h3>
                 <p><a href="lab3/css/custom.css">custom.css</a> </p>
-                <p><a href="lab3/images/wireframe.png">wireframe.png</a></p>
+                <p><a href="lab3/image/wireframe.png">wireframe.png</a></p>
             </section>
 
             <section class="grader-notes">
