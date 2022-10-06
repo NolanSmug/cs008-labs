5075c81bd4670fe0e80e3325f9c939316e828189
Nolan <112602295+NolanSmug@users.noreply.github.com>
2022-10-06 17:25:42 -0400

    Lab 5: updated site map

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Nolan's CS008 Website</title>
-        <meta name="author" content="Nolan Cyr">
-        <meta name="description" content="A site map to all of my assignments for CS 008">
-        <style>
-            
-            body{
+ body{
                 margin: auto;
                 padding: 3%;
                 width: 90%;
@@ -39,93 +30,37 @@
             .small {
                 width: 20%;
             }
-    
-        </style>
-    </head>
+            
+            /* Setting up a grid layout for the main inded page */
+            .header{
+                grid-area: header;
+                grid-column: 1 / 3;
+                padding: .5%;
+                margin: .5%;
+            }  
+            .public-files{
+                grid-area: public-files;
+                padding: .5%;
+                margin: .5%;
+            }
 
-    <body>
-        <figure class="right small">
-            <img alt="Nolan Cyr circa 2022" src="image/nolan-cyr.png">
-            <figcaption>Walking around Roosevelt Island in NYC</figcaption>
-        </figure>
-
-        <h1>CS 008-<em>A</em> Fall 2022</h1>
-        <h2>Nolan Cyr - Site map</h2>
-        <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
-        
-<!----------------------------Lab 4------------------------------------>
-        <section class="grid-layout">
-            <h2 class="header">Lab Four- Basic CSS</h2>
-            <section class="public-files">
-                <h3>Public Files</h3>
-                <p><a href="lab4/detail.php">detail.php</a></p> 
-		        <p><a href="lab4/form.php">form.php</a></p> 
-		        <p><a href="lab4/index.php">index.php</a></p>
-            </section>
-
-            <section class="supporting-files">
-                <h3>Supporting files</h3>
-                <p><a href="lab4/css/custom.css">custom.css</a> </p>
-                <p><a href="lab4/image/wireframe.png">wireframe.png</a></p>
-            </section>
-
-            <section class="grader-notes">
-                <h3>Notes to grader</h3>
-                <p></p>
-            </section>
-        </section>
-<!----------------------------Lab 4------------------------------------>
-
-<!----------------------------Lab 3------------------------------------>
-        <section class="grid-layout">
-            <h2 class="header">Lab Three - Adding detail and a form.</h2>
-            <section class="public-files">
-                <h3>Public Files</h3>
-                <p><a href="lab3/detail.php">detail.php</a></p> 
-		        <p><a href="lab3/form.php">form.php</a></p> 
-		        <p><a href="lab3/index.php">index.php</a></p>
-            </section>
-
-            <section class="supporting-files">
-                <h3>Supporting files</h3>
-                <p><a href="lab3/css/custom.css">custom.css</a> </p>
-                <p><a href="lab3/image/wireframe.png">wireframe.png</a></p>
-            </section>
-
-            <section class="grader-notes">
-                <h3>Notes to grader</h3>
-                <p></p>
-            </section>
-        </section>
-<!----------------------------Lab 3------------------------------------>
-
-<!----------------------------Lab 2------------------------------------>
-        <section class="grid-layout">
-            <h2 class="header">Lab Two - Choosing your topic.</h2>
-            <section class="public-files">
-                <h3>Public Files</h3>
-                <p><a href="lab2/index.php">index.php</a></p>      
-            </section>
-
-            <section class="supporting-files">
-                <h3>Supporting files</h3>
-            </section>
-
-            <section class="grader-notes">
-                <h3>Notes to grader</h3>
-            </section>
-        </section>
-<!----------------------------Lab 2------------------------------------>
-
-<!----------------------------Lab 1------------------------------------>
-        <section class="grid-layout">
-            <h2>Lab one - Setting up your sitemap</h2>
-            <section class="public-files">
-                <h3>Public Files</h3>
-                <p><a href="test.php">Testing Page</a></p>
-            </section>
-        </section>
-<!----------------------------Lab 1------------------------------------>
-
-    </body>
-</html>
+            .supporting-files{
+                grid-area: supporting-files;
+                padding: .5%;
+                margin: .5%;
+            }
+            .grader-notes{
+                grid-area: grader-notes;
+                padding: .5%;
+                margin: .5%;
+            }
+            .grid-layout{
+                border-bottom: thin dashed navy;
+                display: inline-grid;
+                grid-template-columns: 25% 25% 50%;
+                grid-template-areas: "header header header"
+                    "public-files supporting-files grader-notes"; 
+                padding: .5%;
+                margin: .5%;
+                width: 100%;    
+            }
