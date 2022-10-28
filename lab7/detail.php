6ca1f56fa0008080c026eddb3d9b8fd4f2c53754
Nolan <112602295+NolanSmug@users.noreply.github.com>
2022-10-28 12:33:18 -0400

    Lab 7: seperate files for includes

---
+++ lab7/detail.php
@@ -0,0 +1,44 @@
+<?php 
+include 'top.php';
+?>
+    <main class="mainPage">
+        <section class="carsTandB">
+            <h2>Cars, Trucks, and Buses</h2>
+            <p>Road vehicles are among the most common forms of transportaiton, especially in the United States. Light duty cars and heavier duty trucks and buses made 
+                up 83% of our transportation greenhouse gas emissions in 2020 (EPA).  Out of every other country, American citizens drive the highest average of miles per 
+                year (13,474), with Canada being second most at 9,562 miles per year. Part of the reason America is an outlier in this statistic is partly because
+                our infrastructure has been built to favor road transport. There is over 6.8 million miles of road in the United States alone, which is also higher than 
+                every other country (preceding India by almost half a million miles of road).
+            </p>
+        </section>
+        
+        <section class="airplanes">
+            <h2>Airplanes</h2>
+            <p>Although airplanes also release heavy amounts of carbon dioxide into the atmosphere, jet engines also release water vapor, nitrous gases, and particles that
+            reduce AQI (air quality). Water vapor itself does not live long in the atmosphere, however contrails that form from planes are the bigger issue. Under low temperatures,
+            water vapor released from jet engines freezes creating lined contrails. These clouds stretch across the sky and trap infrared rays warming up the atmosphere 3 times more 
+            than carbon dioxide.</p>
+        </section>
+
+        <section class="airplanesImage">
+            <h2> Contrails Left Behind an Airplane </h2> 
+            <figure class="contrailsImg">
+                <img src="images/contrails.jpeg"  alt="Contrails Photo"> 
+                <figcaption>Several parallel contrails left behind a plane
+                    <cite>Photo by Jeff Overton</cite>
+                </figcaption>
+            </figure>
+        </section>
+
+        <section class="trains">
+            <h2>Trains</h2>
+            <p>In comparrison to cars and airplanes, trains emit over 60% less carbon, yet the United States lacks a developed rail system. According to the EPA (Environmental Protection
+                Agency), freight trains made up just 1.9% of our transportation greenhouse gas emissions. Trains are both fuel efficient and don't create traffic and noise pollution like 
+                trucks. If we were to replace every truck transport system in place currently with rail transit, we would cut our transportation GHG emissions by almost 75% (EPA).</p> 
+        </section>
+
+    </main>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
